3605015349ef6197ed65c37152bced30dbcdd018
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-04 16:17:15 -0500

    feat: initial attempt — combine read/process/output in one sequential process

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/looselytyped/going-further
+
+go 1.14
+
+require golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main // import "github.com/looselytyped/going-further"
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println("This is the main program")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		hash, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Printf("%x\n", hash)
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "error", err)
+		}
+	}
 }
