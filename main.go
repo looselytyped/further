6b14998d503c74461ddb0d782625c87ee6442316
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-04 16:20:51 -0500

    feat: [bad] use a channel to separate out reading from processing

--- main.go
+++ main.go
@@ -3,26 +3,29 @@ package main // import "github.com/looselytyped/going-further"
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	inputs := make(chan string)
+
+	defer close(inputs)
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		s := scanner.Text()
+		inputs <- scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 
-		hash, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
-		if err != nil {
-			log.Println(err)
+	for {
+		in, ok := <-inputs
+		if !ok {
+			break
 		}
-
+		hash, _ := bcrypt.GenerateFromPassword([]byte(in), bcrypt.DefaultCost)
 		fmt.Printf("%x\n", hash)
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "error", err)
-		}
 	}
 }
